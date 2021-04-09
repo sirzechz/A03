604a9da6c33b6398cff3197dfd013b392031131c
Tarvarma <t.lecaros01@gmail.com>
2021-04-09 20:46:12 +0000

    Cambios en clase Bandido

--- Bandido.java
+++ Bandido.java
@@ -1,3 +1,12 @@
 public class Bandido{
-    private String nombre;
+    private String apodo;
+    private int peso;
+    private boolean tieneTatuaje;
+    private int salud;
+
+    public void golpear(Bandido b){
+        if (b.salud > 0){
+            b.salud--;
+        }
+    }
 }
