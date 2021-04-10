8c1e0ee6f63aa664f3ac5ebd51a9af334ed3e53e
Tarvarma <53281014+Tarvarma@users.noreply.github.com>
2021-04-10 19:47:52 -0400

    Arreglado error de logica

--- main.java
+++ main.java
@@ -5,7 +5,7 @@ public class main {
     Bandido juan = new Bandido("Juancho", 85, true);
     Bandido miguel = new Bandido("asTa", 543, false);
 
-    while (juan.getSalud() >= 0 || miguel.getSalud() >= 0){
+    while (juan.getSalud() >= 0 && miguel.getSalud() >= 0){
         juan.disparar(miguel);
         miguel.golpear(juan);
     }
